1d5f7b136133a735b8268ab4f5bd585b9b6c2579
Gael Arrambide <gael_arrambide@hotmail.com>
2016-08-19 19:36:13 -0500

    reset after lost

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -37,13 +37,38 @@ var game = {
 	enemiesAvailable:[],
 	yourEnemy: [],
 	currentBattle: [],
-	currentAttack: [],
+	currentAttack: 0,
+	currentHp: 0,
+	currentOpponentH: 0,
+
+	playSequence: function(){
+		game.createPlayers();
+			$(".player").on("click", function (event) {
+				var charInd = $(this).attr("id");
+				game.selectPlayer(charInd);
+				$(".enemy").on("click", function (event) {
+					var charInd2 = $(this).attr("id");
+					game.selectEnemy(charInd, charInd2);
+					$("#attackbutton").on("click", function(){
+						game.attackSequence();
+					});
+				});				
+			});
+	},
 	createPlayers: function(){
 		for (a=0; a<game.playerAvailable.length; a++){
 		$("#yourcharsel").append("<a class='btn btn-success btn-md player' id='" +game.playerAvailable[a].iD+ "'><p id='name'>" + game.playerAvailable[a].name +"</p><img id='pic' src='"+ game.playerAvailable[a].pic +"'><p id='healthPoints'>" + game.playerAvailable[a].hP +"</p></a>");
 		}
 	},
-
+	selectPlayer:function(charInd){
+		console.log(charInd);
+		game.createEnemiesAvailable(charInd);
+		game.afterSel();
+	},
+	selectEnemy:function(charInd, charInd2){
+		game.createEnemy(charInd, charInd2);
+		game.createBattle();
+	},
 	createEnemiesAvailable: function(charInd){
 		for (c=0; c<game.playerAvailable.length; c++){
 			if (charInd===game.playerAvailable[c].iD){
@@ -72,42 +97,61 @@ var game = {
 				$("#enemy").html("<a class='btn btn-success btn-md defender' id='" +game.enemiesAvailable[d].iD+ "'><p id='namee'>" + game.enemiesAvailable[d].name +"</p><img id='pic' src='"+ game.enemiesAvailable[d].pic +"'><p id='healthPointse'>" + game.enemiesAvailable[d].hP +"</p></a>");
 				game.yourEnemy.push(game.enemiesAvailable[d]);
 			} else if(charInd!==charInd2){
-				$("#enemies").append("<a class='btn btn-danger btn-md enemy' id='"+game.enemiesAvailable[d].iD+"'><p id='name'>" + game.enemiesAvailable[d].name +"</p><img id='pic' src='"+ game.enemiesAvailable[d].pic +"'><p id='healthPoints'>" + game.enemiesAvailable[d].hP +"</p></a>");
-				
+				$("#enemies").append("<a class='btn btn-danger btn-md enemy' id='"+game.enemiesAvailable[d].iD+"'><p id='name'>" + game.enemiesAvailable[d].name +"</p><img id='pic' src='"+ game.enemiesAvailable[d].pic +"'><p id='healthPoints'>" + game.enemiesAvailable[d].hP +"</p></a>");	
 			}
-		}
+		}	
+	},
+
+	createBattle: function(){
 		game.currentBattle.push(game.yourPlayer[0]);
 		game.currentBattle.push(game.yourEnemy[0]);
 		console.log(game.currentBattle[0].name+" Vs "+game.currentBattle[1].name);
+		game.currentHp=game.currentBattle[0].hP;
+		game.currentOpponentH= game.currentBattle[1].hP;
+		$("#yourcharsel").html("<a class='btn btn-success btn-md player' id='" +game.currentBattle[0].iD+ "'><p id='name'>" + game.currentBattle[0].name +"</p><img id='pic' src='"+ game.currentBattle[0].pic +"'><p id='healthPoints'>" + game.currentBattle[0].hP +"</p></a>");	
+		$("#enemy").html("<a class='btn btn-success btn-md defender' id='" +game.currentBattle[1].iD+ "'><p id='namee'>" + game.currentBattle[1].name +"</p><img id='pic' src='"+ game.currentBattle[1].pic +"'><p id='healthPointse'>" + game.currentBattle[1].hP +"</p></a>");
 	},
-	attackSequence: function(){		
-		$("#messages").html("<p>You attacked "+ game.currentBattle[1].name +" for "+ game.currentBattle[0].aP+" damage.</p>"+"<p>"+game.currentBattle[1].name + " attacked you back for " + game.currentBattle[1].cP+".</p>");
+
+	attackSequence: function(){
+		game.currentAttack=game.currentAttack+game.currentBattle[0].aP;
+		console.log("Attack by: "+game.currentAttack);
+		game.currentHp=game.currentHp-game.currentBattle[1].cP;
+		console.log("Current health: "+ game.currentHp);
+		game.currentOpponentH=game.currentOpponentH - game.currentAttack;
+		console.log("Opponent health: "+ game.currentOpponentH);
+		if(game.currentHp<=0){
+			$("#messages").html("<p> You have been defeated... GAME OVER!!!</p>");
+			$("#messages").append("<a class='btn btn-info btn-md' id='restart'>Restart</a>");
+			$("#restart").on("click",function(event){
+				game.restart();
+			});
+		} else if (game.currentOpponentH<=0) {
+			$("#messages").html("<p> You have defeated "+game.currentBattle[1].name+ ", you can choose to fight another enemy.</p>");
+			$("#enemy").html(" ");
+		} else{
+			$("#messages").html("<p>You attacked "+ game.currentBattle[1].name +" for "+ game.currentAttack +" damage.</p>"+"<p>"+game.currentBattle[1].name + " attacked you back for " + game.currentBattle[1].cP+".</p>");
+			$("#enemy").html("<a class='btn btn-success btn-md defender' id='" +game.currentBattle[1].iD+ "'><p id='namee'>" + game.currentBattle[1].name +"</p><img id='pic' src='"+ game.currentBattle[1].pic +"'><p id='healthPointse'>" + game.currentOpponentH +"</p></a>");
+		}
+				$("#yourcharsel").html("<a class='btn btn-success btn-md player' id='" +game.currentBattle[0].iD+ "'><p id='name'>" + game.currentBattle[0].name +"</p><img id='pic' src='"+ game.currentBattle[0].pic +"'><p id='healthPoints'>" + game.currentHp +"</p></a>");	
+	},
+	restart: function(){
+		$("#yourcharsel").html("");
+		$("#enemy").html("");
+		$("#enemies").html("");
+		$("#messages").html("");
+		game.yourPlayer=[];
+		game.enemiesAvailable=[];
+		game.yourEnemy=[];
+		game.currentBattle=[];
+		game.currentAttack=0;
+		game.currentHp=0;
+		game.currentOpponentH=0;
+		game.playSequence();
 	}
 }
 
 $(document).ready(function(){
-	game.createPlayers();
-
-	$(".player").on("click", function (event) {
-		var charInd = $(this).attr("id");
-		console.log(charInd);
-		game.createEnemiesAvailable(charInd);
-		game.afterSel();
-			$(".enemy").on("click", function (event) {
-				console.log("createenemy");
-				var charInd2 = $(this).attr("id");
-				console.log(charInd2);
-				game.createEnemy(charInd, charInd2);
-					$("#attackbutton").on("click", function(){
-						console.log("attack button used");
-						game.attackSequence();
-				});
-			});			
-				
-});
-
 	
-
-	
-	
+	game.playSequence();
+		
 });
